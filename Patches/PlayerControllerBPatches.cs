5cccc7e340ca8457d379acf9c72ad643be490e1e
TonyGamer <tomblackaz@gmail.com>
2024-04-05 07:45:53 -0700

    Initial commit

---
+++ Patches/PlayerControllerBPatches.cs
@@ -0,0 +1,49 @@
+using GameNetcodeStuff;
+using HarmonyLib;
+using Steamworks;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace SteamJoinFix.Patches;
+
+[HarmonyPatch(typeof(PlayerControllerB))]
+public class PlayerControllerBPatches
+{
+	[HarmonyDebug]
+	[HarmonyPatch("SendNewPlayerValuesServerRpc")]
+	[HarmonyTranspiler]
+	static IEnumerable<CodeInstruction> SyncUnlockablesCrashPatch(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+	{
+		return new CodeMatcher(instructions, generator)
+			.MatchForward(false,
+					new CodeMatch(OpCodes.Callvirt, AccessTools.Method(typeof(NetworkManager), "DisconnectClient", parameters: new Type[] {typeof(ulong)})))
+			.InsertAndAdvance(
+					new CodeInstruction(OpCodes.Ldarg_0), // this
+					new CodeInstruction(OpCodes.Ldarg_1), // newPlayerSteamId
+					new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(PlayerControllerBPatches), "WaitForSteamID")))
+			.InstructionEnumeration();
+	}
+
+	public static void WaitForSteamID(PlayerControllerB player, ulong newPlayerSteamId)
+	{
+		player.StartCoroutine(WaitForSteamIDCoroutine(player, newPlayerSteamId));
+	}
+
+	private static IEnumerator WaitForSteamIDCoroutine(PlayerControllerB player, ulong newPlayerSteamId)
+	{
+		float timer = 0f;
+		while(timer < 10f)
+		{
+			timer += Time.deltaTime;
+			if (GameNetworkManager.Instance.steamIdsInLobby.Contains(newPlayerSteamId))
+			{
+				AccessTools.Method(typeof(PlayerControllerB), "SendNewPlayerValuesServerRpc").Invoke(player, new object[] {(object) newPlayerSteamId});
+			}
+			yield return null;
+		}
+	}
+}
---
+++ Plugin.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+using HarmonyLib;
+using SteamJoinFix.Patches;
+
+namespace SteamJoinFix;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+	private void Awake()
+	{
+	    Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+	    Harmony.CreateAndPatchAll(typeof(PlayerControllerBPatches));
+	}
+}
